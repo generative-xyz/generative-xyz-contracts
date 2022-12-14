5462659700fbc61e34cd6efeb148e880ffc9fc8a
thaibao-incognito <thaibao@autonomous.nyc>
2022-12-14 15:01:06 +0700

    - add script

--- migrations/data/generativeProjectData/generativeProjectData.ts
+++ migrations/data/generativeProjectData/generativeProjectData.ts
@@ -4,7 +4,7 @@ import {Bytes32Ty} from "hardhat/internal/hardhat-network/stack-traces/logger";
 import {ethers as eth1} from "ethers";
 
 const {ethers, upgrades} = require("hardhat");
-const hardhatConfig = require("../../hardhat.config");
+const hardhatConfig = require("../../../hardhat.config");
 
 class GenerativeProjectData {
 
@@ -144,6 +144,27 @@ class GenerativeProjectData {
 
         const val: any = await temp?.nftContract.methods.tokenBaseURI(projectId, tokenId, seed).call(tx);
         return val;
+    }
+
+    async changeProjectAddress(contractAddress: any, newAddr: any, gas: any) {
+        let temp = this.getContract(contractAddress);
+        const nonce = await temp?.web3.eth.getTransactionCount(this.senderPublicKey, "latest") //get latest nonce
+
+        const fun = temp?.nftContract.methods.changeProjectAddress(newAddr)
+        //the transaction
+        const tx = {
+            from: this.senderPublicKey,
+            to: contractAddress,
+            nonce: nonce,
+            gas: gas,
+            data: fun.encodeABI(),
+        }
+
+        if (tx.gas == 0) {
+            tx.gas = await fun.estimateGas(tx);
+        }
+
+        return await this.signedAndSendTx(temp?.web3, tx);
     }
 }
 
