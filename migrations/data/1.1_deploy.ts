82138ff59e9485f368697a6f099e47a0ae0ea677
thaibao-incognito <thaibao@autonomous.nyc>
2022-12-14 14:20:12 +0700

    - add script

---
+++ migrations/data/1.1_deploy.ts
@@ -0,0 +1,22 @@
+import * as dotenv from 'dotenv';
+import {GenerativeProjectData} from "./generativeProjectData";
+
+
+(async () => {
+    try {
+        if (process.env.NETWORK != "local") {
+            console.log("wrong network");
+            return;
+        }
+        const nft = new GenerativeProjectData(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
+        const address = await nft.deployUpgradeable(
+            process.env.PUBLIC_KEY,
+            "0x46C02B9113DcA70a8C2e878Df0B24Dc895836b75",
+            "",
+        );
+        console.log("%s GenerativeProjectData address: %s", process.env.NETWORK, address);
+    } catch (e) {
+        // Deal with the fact the chain failed
+        console.log(e);
+    }
+})();
---
+++ migrations/data/4.3_tokenHTML.ts
@@ -0,0 +1,26 @@
+import * as dotenv from 'dotenv';
+
+import {ethers} from "ethers";
+import * as fs from "fs";
+import {keccak256} from "ethers/lib/utils";
+import {GalaxyData} from "./galaxydata";
+import Web3 from "web3";
+import {createAlchemyWeb3} from "@alch/alchemy-web3";
+
+const hardhatConfig = require("../../../hardhat.config");
+
+(async () => {
+    try {
+        if (process.env.NETWORK != "mumbai") {
+            console.log("wrong network");
+            return;
+        }
+        const data = new GalaxyData(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
+        const args = process.argv.slice(2)
+        const html = await data.tokenHTML(args[0], args[1]);
+        console.log(html);
+    } catch (e) {
+        // Deal with the fact the chain failed
+        console.log(e);
+    }
+})();
