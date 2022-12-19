e27a5b518dc85bd30d9217488255037f71d7bf34
thaibao-incognito <thaibao@autonomous.nyc>
2022-12-19 10:45:41 +0700

    - add more script

--- migrations/nfts/generativeProject/generativeProject.ts
+++ migrations/nfts/generativeProject/generativeProject.ts
@@ -172,6 +172,27 @@ class GenerativeProject {
         return await this.signedAndSendTx(temp?.web3, tx);
     }
 
+    async updateProjectScript(contractAddress: any, projectId: any, index: any, script: any, gas: any) {
+        let temp = this.getContract(contractAddress);
+        const nonce = await temp?.web3.eth.getTransactionCount(this.senderPublicKey, "latest") //get latest nonce
+
+        const fun = temp?.nftContract.methods.updateProjectScript(projectId, index, script)
+        //the transaction
+        const tx = {
+            from: this.senderPublicKey,
+            to: contractAddress,
+            nonce: nonce,
+            gas: gas,
+            data: fun.encodeABI(),
+        }
+
+        if (tx.gas == 0) {
+            tx.gas = await fun.estimateGas(tx);
+        }
+
+        return await this.signedAndSendTx(temp?.web3, tx);
+    }
+
     async projectDetails(contractAddress: any, tokenID: any) {
         let temp = this.getContract(contractAddress);
         const nonce = await temp?.web3.eth.getTransactionCount(this.senderPublicKey, "latest") //get latest nonce
