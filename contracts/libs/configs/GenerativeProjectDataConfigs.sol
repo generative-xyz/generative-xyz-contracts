bdf14b179bfa4a3be7233d8c33ab075932a08501
thaibao-incognito <thaibao@autonomous.nyc>
2022-12-19 09:29:57 +0700

    - update contract

--- contracts/libs/configs/GenerativeProjectDataConfigs.sol
+++ contracts/libs/configs/GenerativeProjectDataConfigs.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.8.0;
 library GenerativeProjectDataConfigs {
     string public constant BASE_URI = "BASE_URI";
     string public constant BASE_URI_TRAIT = "BASE_URI_TRAIT";
+    string public constant DIV_CONTAINER = "DIV_CONTAINER";
+    string public constant RANDOM_FUNC_SCRIPT = "TOKEN_SEED_SCRIPT";
 }
