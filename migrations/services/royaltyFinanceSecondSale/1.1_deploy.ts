da14bcc36058640f5bceeec249761e0778b6eedc
thaibao-incognito <thaibao@autonomous.nyc>
2022-12-19 14:57:42 +0700

    - add more script

---
+++ migrations/services/royaltyFinanceSecondSale/1.1_deploy.ts
@@ -0,0 +1,22 @@
+import * as dotenv from 'dotenv';
+import {RoyaltyFinanceSecondSale} from "./royaltyFinanceSecondSale";
+
+
+(async () => {
+    try {
+        if (process.env.NETWORK != "mumbai") {
+            console.log("wrong network");
+            return;
+        }
+        const nft = new RoyaltyFinanceSecondSale(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
+        const address = await nft.deployUpgradeable(
+            process.env.PUBLIC_KEY,
+            "0x47B528E9eDD8f7Dd709bCa9f7E45c499C85eccfb",
+            "0x12E258A3307DeDDb26478D274a3C9343cf9107D6",
+            "0x0000000000000000000000000000000000000000");
+        console.log("%s RoyaltyFinanceSecondSale address: %s", process.env.NETWORK, address);
+    } catch (e) {
+        // Deal with the fact the chain failed
+        console.log(e);
+    }
+})();
