08a4d4f9f1f05aec6bfc25c19e9650d6602394dc
thaibao-incognito <thaibao@autonomous.nyc>
2023-01-09 15:00:05 +0700

    - add note

--- migrations/services/marketplace/2.6_setApproveERC20MakeOffer.ts
+++ migrations/services/marketplace/2.6_setApproveERC20MakeOffer.ts
@@ -15,6 +15,11 @@ import dayjs = require("dayjs");
         const marketplaceService = new AdvanceMarketplaceService(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
         const args = process.argv.slice(2)
         console.log(args);
+
+        // support WETH
+        // goerli https://goerli.etherscan.io/token/0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6
+        // mumbai https://mumbai.polygonscan.com/token/0xa6fa4fb5f76172d178d61b04b0ecd319c5d1c0aa
+        // mainnet https://etherscan.io/token/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
         const tx = await marketplaceService.setApproveERC20MakeOffer(args[0], args[1], true, 0);
         console.log("tx:", tx?.transactionHash, tx?.status);
     } catch (e) {
