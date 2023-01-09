69749cc0ae9fb86c478a1c6df8bcf4ef3ca491f3
thaibao-incognito <thaibao@autonomous.nyc>
2023-01-09 15:28:26 +0700

    - Prepare GENToken.sol

--- contracts/erc20/GENToken.sol
+++ contracts/erc20/GENToken.sol
@@ -1,12 +1,51 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/presets/ERC20PresetMinterPauserUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract GENToken is ERC20Upgradeable {
+import "../libs/helpers/Errors.sol";
+
+contract GENToken is Initializable, ERC20PresetMinterPauserUpgradeable, OwnableUpgradeable {
+    address public _admin;
+
     function initialize(
         string memory name,
-        string memory symbol
+        string memory symbol,
+        address admin,
+        uint256 initSupply
     ) initializer public {
-        __ERC20_init(name, symbol);
+        require(admin != Errors.ZERO_ADDR, Errors.INV_ADD);
+        _admin = admin;
+
+        // init supply
+        // 100 mil with decimals = 4 for testnet
+        // 0 with decimal = 4 for mainnet
+        uint256 _totalSupplyRove = initSupply;
+        mint(admin, _totalSupplyRove);
+
+        __ERC20PresetMinterPauser_init(name, symbol);
+        __Ownable_init();
+    }
+
+    function changeAdmin(address _newAdmin) external {
+        require(_newAdmin != Errors.ZERO_ADDR && _newAdmin != _admin, Errors.INV_ADD);
+        _admin = _newAdmin;
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 4;
+    }
+
+    /** ControlSupply
+    **/
+    function mint(address to, uint256 amount) public whenNotPaused override {
+        require(msg.sender == _admin, Errors.ONLY_ADMIN_ALLOWED);
+        _mint(to, amount);
+    }
+
+    function mintByBlock(address to) external whenNotPaused virtual {
+        // TODO
+        require(1 == 0);
+        _mint(to, 0);
     }
 }
