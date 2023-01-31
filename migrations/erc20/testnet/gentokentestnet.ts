6c69f089ae64c28e4f06e881ce34a53a71f1fbc1
genesis-0000 <thaibao@autonomous.nyc>
2023-01-31 10:06:43 +0700

    - add more script

--- migrations/erc20/testnet/gentokentestnet.ts
+++ migrations/erc20/testnet/gentokentestnet.ts
@@ -107,6 +107,27 @@ class GENTokenTestnet {
         return await this.signedAndSendTx(temp?.web3, tx);
     }
 
+    async setProxyPoASecondSale(contractAddr: any, proxyPoA: any, approve: boolean, gas: any) {
+        let temp = this.getContract(contractAddr);
+        const nonce = await temp?.web3.eth.getTransactionCount(this.senderPublicKey, "latest") //get latest nonce
+
+        const fun = temp?.nftContract.methods.setProxyPoASecondSale(proxyPoA, approve);
+        //the transaction
+        const tx = {
+            from: this.senderPublicKey,
+            to: contractAddr,
+            nonce: nonce,
+            gas: gas,
+            data: fun.encodeABI(),
+        }
+
+        if (tx.gas == 0) {
+            tx.gas = await fun.estimateGas(tx);
+        }
+
+        return await this.signedAndSendTx(temp?.web3, tx);
+    }
+
     async totalSupply(contractAddress: any) {
         let temp = this.getContract(contractAddress);
         const nonce = await temp?.web3.eth.getTransactionCount(this.senderPublicKey, "latest") //get latest nonce
