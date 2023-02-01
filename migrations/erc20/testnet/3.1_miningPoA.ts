0e761ce3ea0596a25488d029d360815396fcdefa
genesis-0000 <thaibao@autonomous.nyc>
2023-02-01 14:30:10 +0700

    - check mining PoA

--- migrations/erc20/testnet/3.1_miningPoA.ts
+++ migrations/erc20/testnet/3.1_miningPoA.ts
@@ -2,17 +2,20 @@ import {GENTokenTestnet} from "./gentokentestnet";
 
 (async () => {
     try {
-        if (process.env.NETWORK != "mumbai") {
+        if (process.env.NETWORK != "goerli") {
             console.log("wrong network");
             return;
         }
         const args = process.argv.slice(2);
         const contractPro = args[0];
         const genProjectAddr = args[1];
-        const genProjectId = args[2];
+        let genProjectId = args[2];
         const erc20 = new GENTokenTestnet(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
+        // for (let i = 7; i <= 14; i++) {
+        // genProjectId = i;
         const tx = await erc20.miningPoA(contractPro, genProjectAddr, genProjectId, 0);
         console.log("Proof of Art minning GENToken ", tx?.transactionHash, tx?.status);
+        // }
     } catch (e) {
         // Deal with the fact the chain failed
         console.log(e);
