490dd54f7d91872073898a5f79d4cc141ddd6240
genesis-0000 <thaibao@autonomous.nyc>
2023-03-01 14:00:48 +0700

    - update

---
+++ contracts/services/Authentic.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
+
+import "../libs/helpers/Errors.sol";
+
+contract Authentic is Initializable, OwnableUpgradeable {
+
+    address public _admin; // is a mutil sig address when deploy
+    address public _parameterAddr;
+
+    mapping(address => bool) public _caller;
+    mapping(address => mapping(uint256 => string)) public _authentic;
+
+    function initialize(address admin, address parameterControl) initializer virtual public {
+        require(admin != Errors.ZERO_ADDR, Errors.INV_ADD);
+        require(parameterControl != Errors.ZERO_ADDR, Errors.INV_ADD);
+
+        _admin = admin;
+        _parameterAddr = parameterControl;
+        __Ownable_init();
+    }
+
+    function changeAdmin(address newAdm) external {
+        require(msg.sender == _admin && newAdm != Errors.ZERO_ADDR, Errors.ONLY_ADMIN_ALLOWED);
+
+        if (_admin != newAdm) {
+            _admin = newAdm;
+        }
+    }
+
+    function setCaller(address caller, bool approved) external {
+        require(msg.sender == _admin, "INV_CALLER");
+        _caller[caller] = approved;
+    }
+
+    function setAuthentic(address coll, uint256 tokenId, string memory inscriptionId) external {
+        IERC721Upgradeable tokenERC721 = IERC721Upgradeable(coll);
+        require(tokenERC721.ownerOf(tokenId) == msg.sender || _caller[msg.sender] || msg.sender == _admin, "INV_CALLER");
+        _authentic[coll][tokenId] = inscriptionId;
+    }
+}
