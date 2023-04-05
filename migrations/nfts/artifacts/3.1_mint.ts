6a820085befabddaa4251c05708bb68a6ef9a544
genesis-0000 <thaibao@autonomous.nyc>
2023-04-05 14:09:26 +0700

    - update

--- migrations/nfts/artifacts/3.1_mint.ts
+++ migrations/nfts/artifacts/3.1_mint.ts
@@ -6,6 +6,11 @@ import {createAlchemyWeb3} from "@alch/alchemy-web3";
 import dayjs = require("dayjs");
 import {Artifacts} from "./artifacts";
 
+function getByteArray(filePath: string) {
+    let fileData = fs.readFileSync(filePath);
+    return fileData;
+}
+
 (async () => {
     try {
         if (process.env.NETWORK != "tc_testnet") {
@@ -16,11 +21,13 @@ import {Artifacts} from "./artifacts";
         const nft = new Artifacts(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
         const args = process.argv.slice(2)
 
+        const result = getByteArray(args[1]);
+
         const contract = args[0];
-        const tx = await nft.mintChunks(
+        const tx = await nft.preserveChunks(
                 contract,
                 process.env.PUBLIC_KEY,
-                ["0x1"],
+                [result],
                 0
             )
         ;
