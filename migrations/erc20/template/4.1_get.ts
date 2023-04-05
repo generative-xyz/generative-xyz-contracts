d7f55ed2a68f44e157caa47f69ea8ffce5b83e17
genesis-0000 <thaibao@autonomous.nyc>
2023-04-05 16:06:54 +0700

    - update

--- migrations/erc20/template/4.1_get.ts
+++ migrations/erc20/template/4.1_get.ts
@@ -10,8 +10,9 @@ import {ERC20Template} from "./ERC20Template";
         const contract = args[0];
         const token = new ERC20Template(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
         let a: any = {};
-        // a.totalSupply = await token.totalSupply(contract);
-        a.proofOfArtAvailable = await token.proofOfArtAvailable(contract, args[1], args[2]);
+        a.totalSupply = await token.totalSupply(contract);
+        a.balanceOf = await token.balanceOf(contract, args[1]);
+        a.name = await token.name(contract);
         console.log({a});
     } catch (e) {
         // Deal with the fact the chain failed
