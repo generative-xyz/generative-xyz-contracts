ba03876fa8401178c9a44985347d650bc4ebdf88
genesis-0000 <thaibao@autonomous.nyc>
2023-04-05 14:23:10 +0700

    - rename

--- contracts/erc20/ERC20Token.sol
+++ contracts/erc20/ERC20Token.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract ERC20Template is ERC20, ERC20Burnable, Ownable {
+contract ERC20Token is ERC20, ERC20Burnable, Ownable {
     constructor(string memory name, string memory symbol, uint256 initSupply) ERC20(name, symbol) {
         _mint(msg.sender, initSupply * 10 ** decimals());
     }
