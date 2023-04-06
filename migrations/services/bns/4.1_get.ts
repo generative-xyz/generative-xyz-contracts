e113e19770382131127d8c6c402a772fb4b1ee57
genesis-0000 <thaibao@autonomous.nyc>
2023-04-06 09:51:56 +0700

    - update

---
+++ migrations/services/bns/4.1_get.ts
@@ -0,0 +1,31 @@
+import * as dotenv from 'dotenv';
+
+import {ethers} from "ethers";
+import * as fs from "fs";
+import {keccak256} from "ethers/lib/utils";
+import Web3 from "web3";
+import {createAlchemyWeb3} from "@alch/alchemy-web3";
+import {Bns} from "./bfs";
+
+(async () => {
+    try {
+        if (process.env.NETWORK != "tc_testnet") {
+            console.log("wrong network");
+            return;
+        }
+        const data = new Bns(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
+        const args = process.argv.slice(2)
+
+        const registered = await data.registered(args[0], Buffer.from(args[1], "utf-8"));
+        if (registered) {
+            const tokenId = await data.registry(args[0], Buffer.from(args[1], "utf-8"));
+            const resolver = await data.resolver(args[0], tokenId);
+            console.log({tokenId}, {resolver});
+        } else {
+            return registered
+        }
+    } catch (e) {
+        // Deal with the fact the chain failed
+        console.log(e);
+    }
+})();
