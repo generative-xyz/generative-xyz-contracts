4845f77cd3caca95af0ebc3a7e6ff62bd97fbb98
genesis-0000 <thaibao@autonomous.nyc>
2023-04-06 09:07:56 +0700

    - update

--- migrations/erc20/template/1.1_deploy.ts
+++ migrations/erc20/template/1.1_deploy.ts
@@ -4,14 +4,14 @@ import {ERC20Template} from "./ERC20Template";
 
 (async () => {
     try {
-        if (process.env.NETWORK != "tc_mainnet") {
+        if (process.env.NETWORK != "tc_testnet") {
             console.log("wrong network");
             return;
         }
         const erc20 = new ERC20Template(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
         const address = await erc20.deploy(
-            "ART",
-            "ART",
+            "Metaverse",
+            "META",
             100000000
         );
         console.log("%s GENToken address: %s", process.env.NETWORK, address);
