f812790620716b39aa6a8b3f035a9533dd2703c5
genesis-0000 <thaibao@autonomous.nyc>
2023-04-14 13:07:05 +0700

    - update

--- contracts/data/GenerativeProjectData.sol
+++ contracts/data/GenerativeProjectData.sol
@@ -145,9 +145,9 @@ contract GenerativeProjectData is OwnableUpgradeable, IGenerativeProjectData {
                     Base64.encode(abi.encodePacked(
                         '{"name":"', ctx._name,
                         '","description": "', ctx._desc, '"',
-                        ', "image": "', ctx._baseURI, '?capture=60000"',
+                        ', "image": "', ctx._baseURI, '&capture=60000"',
                         ctx._animationURI,
-                        ', "attributes": "', ctx._baseURI, '?capture=0"',
+                        ', "attributes": "', ctx._baseURI, '&capture=0"',
                         '}'
                     ))
                 )
