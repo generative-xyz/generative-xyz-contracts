34eed5df7080bcfb98f8c42b9a49c1542ee8fa06
genesis-0000 <thaibao@autonomous.nyc>
2023-04-24 15:18:25 +0700

    - update

--- migrations/services/marketplace/1.1_deploy.ts
+++ migrations/services/marketplace/1.1_deploy.ts
@@ -4,12 +4,12 @@ import {AdvanceMarketplaceService} from "./advanceMarketplaceService";
 
 (async () => {
     try {
-        if (process.env.NETWORK != "mumbai") {
+        if (process.env.NETWORK != "tc_testnet") {
             console.log("wrong network");
             return;
         }
         const marketplace = new AdvanceMarketplaceService(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
-        const address = await marketplace.deployUpgradeable(process.env.PUBLIC_KEY, "0x47B528E9eDD8f7Dd709bCa9f7E45c499C85eccfb");
+        const address = await marketplace.deployUpgradeable(process.env.PUBLIC_KEY, "0x984eEd0C15353bA88d8f35FD929e260bf70d03BD");
         console.log("%s AdvanceMarketplaceService address: %s", process.env.NETWORK, address);
     } catch (e) {
         // Deal with the fact the chain failed
