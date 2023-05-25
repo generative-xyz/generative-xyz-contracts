24fd32f5f7ec14bc8a5661945766255ffe8cb558
genesis-0000 <thaibao@autonomous.nyc>
2023-05-25 16:56:25 +0700

    - script

---
+++ migrations/nfts/solaris/1.2_upgradeContract.ts
@@ -0,0 +1,17 @@
+import {Solaris} from "./Solaris";
+
+(async () => {
+    try {
+        if (process.env.NETWORK != "mumbai") {
+            console.log("wrong network");
+            return;
+        }
+        const args = process.argv.slice(2);
+        const nft = new Solaris(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
+        const address = await nft.upgradeContract(args[0]);
+        console.log({address});
+    } catch (e) {
+        // Deal with the fact the chain failed
+        console.log(e);
+    }
+})();
