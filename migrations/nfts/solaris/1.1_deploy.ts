b1763c68d1e3326745181c4cb67ab2acb5a92d29
genesis-0000 <thaibao@autonomous.nyc>
2023-05-25 16:54:30 +0700

    - update

--- migrations/nfts/solaris/1.1_deploy.ts
+++ migrations/nfts/solaris/1.1_deploy.ts
@@ -14,7 +14,7 @@ import {Solaris} from "./Solaris";
             "0xdc913D967D6bD734Cc435cDf139E60a6828030EC",
             "0xaA320251332e77620317080C9464df9F33291E81",
             "0xACd7aeAe6B4e8B7EF7fc87f0d0F0824DBa9f9927");
-        console.log("%s GenerativeNFT address: %s", process.env.NETWORK, address);
+        console.log("%s Solaris address: %s", process.env.NETWORK, address);
     } catch (e) {
         // Deal with the fact the chain failed
         console.log(e);
