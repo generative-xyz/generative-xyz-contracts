43b1f66f04b6ffffa53985ec399ed9efd1c9c469
genesis-0000 <thaibao@autonomous.nyc>
2023-05-25 11:19:07 +0700

    - soralis

---
+++ contracts/nfts/Solaris.sol
@@ -0,0 +1,160 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/presets/ERC721PresetMinterPauserAutoIdUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/ClonesUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/interfaces/IERC2981Upgradeable.sol";
+
+import "../libs/helpers/Errors.sol";
+import "../libs/structs/Royalty.sol";
+import "../libs/structs/NFTCollection.sol";
+import "../libs/structs/NFTProjectData.sol";
+import "../interfaces/IParameterControl.sol";
+import "../libs/helpers/Base64.sol";
+import "../libs/helpers/StringsUtils.sol";
+import "../libs/configs/GenerativeProjectDataConfigs.sol";
+
+contract Solaris is Initializable, ERC721PausableUpgradeable, ReentrancyGuardUpgradeable, OwnableUpgradeable, IERC2981Upgradeable {
+
+    address public _admin;
+    address public _paramsAddress;
+    address public _randomizerAddr;
+    uint256 private _currentId;
+    string public _script;
+    address public _brc20Token;
+
+    mapping(uint256 => NFTCollection.OwnerSeed) internal _ownersAndHashSeeds;
+
+    function initialize(
+        string memory name,
+        string memory symbol,
+        address admin,
+        address paramsAddress,
+        address randomizerAddr,
+        address gmToken
+    ) initializer public {
+        require(admin != Errors.ZERO_ADDR, Errors.INV_ADD);
+        require(paramsAddress != Errors.ZERO_ADDR, Errors.INV_ADD);
+        _paramsAddress = paramsAddress;
+        _admin = admin;
+        _randomizerAddr = randomizerAddr;
+        _brc20Token = gmToken;
+
+        __ERC721_init(name, symbol);
+        __ReentrancyGuard_init();
+        __ERC721Pausable_init();
+        __Ownable_init();
+    }
+
+    function changeAdmin(address newAdm) external {
+        require(msg.sender == _admin && newAdm != Errors.ZERO_ADDR, Errors.ONLY_ADMIN_ALLOWED);
+
+        // change admin
+        if (_admin != newAdm) {
+            _admin = newAdm;
+        }
+    }
+
+    function changeParamAddr(address newAddr) external {
+        require(msg.sender == _admin && newAddr != Errors.ZERO_ADDR, Errors.ONLY_ADMIN_ALLOWED);
+
+        // change
+        if (_paramsAddress != newAddr) {
+            _paramsAddress = newAddr;
+        }
+    }
+
+    function changeRandomizerAddr(address newAddr) external {
+        require(msg.sender == _admin && newAddr != Errors.ZERO_ADDR, Errors.ONLY_ADMIN_ALLOWED);
+
+        // change
+        if (_randomizerAddr != newAddr) {
+            _randomizerAddr = newAddr;
+        }
+    }
+
+    function changeScript(string memory newScript) external {
+        require(msg.sender == _admin, Errors.ONLY_ADMIN_ALLOWED);
+        _script = newScript;
+    }
+
+    function changeBrc20Token(address newBrc20) external {
+        require(msg.sender == _admin && newBrc20 != Errors.ZERO_ADDR, Errors.ONLY_ADMIN_ALLOWED);
+
+        if (_brc20Token != newBrc20) {
+            _brc20Token = newBrc20;
+        }
+    }
+
+    function tokenIdToHash(uint256 tokenId) external view returns (bytes32) {
+        require(_exists(tokenId), Errors.INV_TOKEN);
+        if (_ownersAndHashSeeds[tokenId]._seed == 0) {
+            return 0;
+        }
+        return keccak256(abi.encode(_ownersAndHashSeeds[tokenId]._seed));
+    }
+
+    function ownerOf(uint256 tokenId) public view virtual override returns (address) {
+        return _ownersAndHashSeeds[tokenId]._owner;
+    }
+
+    function _exists(uint256 tokenId) internal view virtual override returns (bool) {
+        return _ownersAndHashSeeds[tokenId]._owner != Errors.ZERO_ADDR;
+    }
+
+    function _mint(address to, uint256 tokenId) internal virtual override {
+        super._mint(to, tokenId);
+        _ownersAndHashSeeds[tokenId]._owner = to;
+    }
+
+    function _burn(uint256 tokenId) internal virtual override {
+        super._burn(tokenId);
+        delete _ownersAndHashSeeds[tokenId]._owner;
+    }
+
+    function _transfer(address from, address to, uint256 tokenId) internal virtual override {
+        super._transfer(from, to, tokenId);
+        _ownersAndHashSeeds[tokenId]._owner = to;
+    }
+
+    function tokenURI(uint256 tokenId) override public view returns (string memory result) {
+        require(_exists(tokenId), Errors.INV_TOKEN);
+        bytes32 seed = this.tokenIdToHash(tokenId);
+
+        IParameterControl param = IParameterControl(_paramsAddress);
+        string memory html = this.tokenHTML(seed);
+        NFTProjectData.TokenURIContext memory ctx;
+        ctx._animationURI = string(abi.encodePacked(', "animation_url":"', html, '"'));
+        ctx._baseURI = param.get(GenerativeProjectDataConfigs.BASE_URI_TRAIT);
+        ctx._baseURI = string(abi.encodePacked(ctx._baseURI, "/",
+            StringsUpgradeable.toHexString(address(this)), "/",
+            StringsUpgradeable.toString(tokenId), "?seed=", StringsUtils.toHex(seed)));
+        result = string(
+            abi.encodePacked(
+                'data:application/json;base64,',
+                Base64.encode(abi.encodePacked(
+                    '{"name":"', ctx._name,
+                    '","description": "', Base64.encode(abi.encodePacked(ctx._desc)), '"',
+                    ', "image": "', ctx._baseURI, '&capture=60000"',
+                    ctx._animationURI,
+                    ', "attributes": "', ctx._baseURI, '&capture=0"',
+                    '}'
+                ))
+            )
+        );
+    }
+
+    function tokenHTML(bytes32 seed) external view returns (string memory result) {
+        result = _script;
+    }
+
+    /** @dev EIP2981 royalties implementation. */
+    // EIP2981 standard royalties return.
+    function royaltyInfo(uint256 projectId, uint256 _salePrice) external view override
+    returns (address receiver, uint256 royaltyAmount){
+        receiver = _admin;
+        royaltyAmount = (_salePrice * 500) / Royalty.MINT_PERCENT_ROYALTY;
+    }
+}
