c42b077fd8fa9b6682c2ddf92f167986dbce7095
genesis-0000 <thaibao@autonomous.nyc>
2023-05-25 17:03:33 +0700

    - script

--- migrations/nfts/solaris/3.2_batchMint.ts
+++ migrations/nfts/solaris/3.2_batchMint.ts
@@ -1,3 +1,7 @@
+import * as dotenv from 'dotenv';
+
+import {ethers} from "ethers";
+import {GenerativeProject} from "../generativeProject/generativeProject";
 import {Solaris} from "./Solaris";
 
 (async () => {
@@ -7,9 +11,13 @@ import {Solaris} from "./Solaris";
             return;
         }
         const args = process.argv.slice(2);
+        const contract = args[0];
+        const to = args[1];
+        const n = args[2];
+
         const nft = new Solaris(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
-        const address = await nft.upgradeContract(args[0]);
-        console.log({address});
+        const tx = await nft.batchMint(contract, to, parseInt(n), 0);
+        console.log("tx: ", tx?.transactionHash, tx?.status);
     } catch (e) {
         // Deal with the fact the chain failed
         console.log(e);
