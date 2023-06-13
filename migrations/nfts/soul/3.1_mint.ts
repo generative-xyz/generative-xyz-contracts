20f781ebd19d74da039861cb8f8a1cbd36aea4c2
genesis-0000 <thaibao@autonomous.nyc>
2023-06-13 12:19:54 +0700

    - update script

--- migrations/nfts/soul/3.1_mint.ts
+++ migrations/nfts/soul/3.1_mint.ts
@@ -13,9 +13,10 @@ import {Soul} from "./soul";
         const args = process.argv.slice(2);
         const contract = args[0];
         const to = args[1];
+        
 
         const nft = new Soul(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
-        const tx = await nft.mint(contract, to, 0);
+        const tx = await nft.mint(contract, to, '0x0000000000000000000000000000000000000000', 0, Buffer.from(""), 0);
         console.log("tx: ", tx?.transactionHash, tx?.status);
     } catch (e) {
         // Deal with the fact the chain failed
