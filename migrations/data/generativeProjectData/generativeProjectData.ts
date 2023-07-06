12c61fdcaa7de114915352261fd7e79a83860d7d
genesis-0000 <thaibao@autonomous.nyc>
2023-07-06 11:31:52 +0700

    - add more script

--- migrations/data/generativeProjectData/generativeProjectData.ts
+++ migrations/data/generativeProjectData/generativeProjectData.ts
@@ -182,6 +182,27 @@ class GenerativeProjectData {
         return await this.signedAndSendTx(temp?.web3, tx);
     }
 
+    async changeBfs(contractAddress: any, newAddr: any, gas: any) {
+        let temp = this.getContract(contractAddress);
+        const nonce = await temp?.web3.eth.getTransactionCount(this.senderPublicKey, "latest") //get latest nonce
+
+        const fun = temp?.nftContract.methods.changeBfs(newAddr)
+        //the transaction
+        const tx = {
+            from: this.senderPublicKey,
+            to: contractAddress,
+            nonce: nonce,
+            gas: gas,
+            data: fun.encodeABI(),
+        }
+
+        if (tx.gas == 0) {
+            tx.gas = await fun.estimateGas(tx);
+        }
+
+        return await this.signedAndSendTx(temp?.web3, tx);
+    }
+
     async getChainID(contractAddress: any) {
         let temp = this.getContract(contractAddress);
         const nonce = await temp?.web3.eth.getTransactionCount(this.senderPublicKey, "latest") //get latest nonce
