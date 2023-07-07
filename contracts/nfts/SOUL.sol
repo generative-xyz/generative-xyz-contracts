e7b5eacd93e97366be739abf11e93f4c9eebbd00
genesis-0000 <thaibao@autonomous.nyc>
2023-07-07 11:21:06 +0700

    - fix bug

--- contracts/nfts/SOUL.sol
+++ contracts/nfts/SOUL.sol
@@ -618,7 +618,7 @@ contract SOUL is Initializable, ERC721PausableUpgradeable, ReentrancyGuardUpgrad
         IParameterControl param = IParameterControl(_paramsAddress);
         address SWAP_POOL_GM_ETH_CONTRACT_ADDRESS = param.getAddress("SWAP_POOL_GM_ETH_CONTRACT_ADDRESS");
         result = string(abi.encodePacked(result, "let SWAP_POOL_GM_ETH_CONTRACT_ADDRESS='", StringsUpgradeable.toHexString(SWAP_POOL_GM_ETH_CONTRACT_ADDRESS), "';"));
-        result = string(abi.encodePacked(result, "let soulNftContractAddress='", StringsUpgradeable.toHexString(address(this)), "';"));
+        result = string(abi.encodePacked(result, "let SOUL_NFT_CONTRACT='", StringsUpgradeable.toHexString(address(this)), "';"));
         result = string(abi.encodePacked(result, "let TOKEN_ID='", StringsUpgradeable.toString(tokenId), "';"));
         result = string(abi.encodePacked(result, "</script>"));
     }
