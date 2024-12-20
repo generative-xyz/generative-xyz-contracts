0fd968e2c134e7a4adfa4b50abe386cf71002c56
genesis0000 <genesis0000@bvm.network>
2024-12-20 17:37:05 +0700

    - update

--- migrations/data/cryptoai/cryptoAIData.ts
+++ migrations/data/cryptoai/cryptoAIData.ts
@@ -181,6 +181,49 @@ class CryptoAIData {
         return await this.signedAndSendTx(temp?.web3, tx);
     }
 
+    async addDNAVariantRobot(contractAddress: any, gas: any, name: string[],
+                             trait: number[]) {
+        let temp = this.getContract(contractAddress);
+        const nonce = await temp?.web3.eth.getTransactionCount(this.senderPublicKey, "latest") //get latest nonce
+
+
+        const fun = temp?.nftContract.methods.addDNAVariantRobot(name, trait);
+        //the transaction
+        const tx = {
+            from: this.senderPublicKey,
+            to: contractAddress,
+            nonce: nonce,
+            gas: gas,
+            data: fun.encodeABI(),
+        }
+
+        if (tx.gas == 0) {
+            tx.gas = await fun.estimateGas(tx);
+        }
+
+        return await this.signedAndSendTx(temp?.web3, tx);
+    }
+
+    async addDNAVariantRobotPosition(contractAddress: any, gas: any, positions: number[][], index: number, length: number) {
+        let temp = this.getContract(contractAddress);
+        const nonce = await temp?.web3.eth.getTransactionCount(this.senderPublicKey, "latest") //get latest nonce
+
+        const fun = temp?.nftContract.methods.addDNAVariantRobotPosition(positions, index, length);
+        //the transaction
+        const tx = {
+            from: this.senderPublicKey,
+            to: contractAddress,
+            nonce: nonce,
+            gas: gas,
+            data: fun.encodeABI(),
+        }
+        console.log(positions.length, index, length);
+        if (tx.gas == 0) {
+            tx.gas = await fun.estimateGas(tx);
+        }
+        return await this.signedAndSendTx(temp?.web3, tx);
+    }
+
     async setPallets(contractAddress: any, gas: any, pallets: number[][]) {
         let temp = this.getContract(contractAddress);
         const nonce = await temp?.web3.eth.getTransactionCount(this.senderPublicKey, "latest") //get latest nonce
