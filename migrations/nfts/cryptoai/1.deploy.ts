6c83a39be7db038e5076b413e2d52c32ae4e7515
genesis0000 <genesis0000@bvm.network>
2024-12-20 12:58:36 +0700

    - update crypto ai

--- migrations/nfts/cryptoai/1.deploy.ts
+++ migrations/nfts/cryptoai/1.deploy.ts
@@ -8,9 +8,7 @@ async function main() {
     }
     const config = await initConfig();
     const dataContract = new CryptoAI(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
-    const address = await dataContract.deployUpgradeable("test", "test",
-        process.env.PUBLIC_KEY,
-        process.env.PUBLIC_KEY)
+    const address = await dataContract.deployUpgradeable("test", "test", process.env.PUBLIC_KEY)
     console.log('CryptoAIData contract address:', address);
     await updateConfig("contractAddress", address);
     console.log('Deploy succesful');
