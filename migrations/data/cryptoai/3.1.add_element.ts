e5277fdaefec096a0b2711df7ab1159aa31f7675
genesis0000 <genesis0000@bvm.network>
2024-12-20 14:09:29 +0700

    - update

--- migrations/data/cryptoai/3.1.add_element.ts
+++ migrations/data/cryptoai/3.1.add_element.ts
@@ -43,10 +43,10 @@ async function main() {
             }
         });
 
-        await dataContract.addItem(address, 0, ELEMENT.MOUTH, data.elements.Mouth.names, data.elements.Mouth.traits, data.elements.Mouth.positions);
-        await dataContract.addItem(address, 0, ELEMENT.HEAD, data.elements.Head.names, data.elements.Head.traits, data.elements.Head.positions);
-        await dataContract.addItem(address, 0, ELEMENT.EYES, data.elements.Eyes.names, data.elements.Eyes.traits, data.elements.Eyes.positions);
-        await dataContract.addItem(address, 0, ELEMENT.BODY, data.elements.Body.names, data.elements.Body.traits, data.elements.Body.positions);
+        // await dataContract.addItem(address, 0, ELEMENT.MOUTH, data.elements.Mouth.names, data.elements.Mouth.traits, data.elements.Mouth.positions);
+        // await dataContract.addItem(address, 0, ELEMENT.HEAD, data.elements.Head.names, data.elements.Head.traits, data.elements.Head.positions);
+        // await dataContract.addItem(address, 0, ELEMENT.EYES, data.elements.Eyes.names, data.elements.Eyes.traits, data.elements.Eyes.positions);
+        // await dataContract.addItem(address, 0, ELEMENT.BODY, data.elements.Body.names, data.elements.Body.traits, data.elements.Body.positions);
 
         // Check positions for each DNA variant
         data.DNA.Dog.positions.forEach((pos: any[], index: number) => {
@@ -85,14 +85,14 @@ async function main() {
             }
         });
         // //ADD DNA
-        await dataContract.addDNA(address, 0, KEY_DNA, TRAITS_DNA);
+        // await dataContract.addDNA(address, 0, KEY_DNA, TRAITS_DNA);
         //ADD DNA Variant
-        await dataContract.addDNAVariant(address, 0, DNA.DOG, data.DNA.Dog.names, data.DNA.Dog.traits, data.DNA.Dog.positions);
-        await dataContract.addDNAVariant(address, 0, DNA.CAT, data.DNA.Cat.names, data.DNA.Cat.traits, data.DNA.Cat.positions);
-        await dataContract.addDNAVariant(address, 0, DNA.FROG, data.DNA.Frog.names, data.DNA.Frog.traits, data.DNA.Frog.positions);
-        await dataContract.addDNAVariant(address, 0, DNA.ROBOT, data.DNA.Robot.names, data.DNA.Robot.traits, data.DNA.Robot.positions);
-        await dataContract.addDNAVariant(address, 0, DNA.HUMAN, data.DNA.Human.names, data.DNA.Human.traits, data.DNA.Human.positions);
-        await dataContract.addDNAVariant(address, 0, DNA.MONKEY, data.DNA.Monkey.names, data.DNA.Monkey.traits, data.DNA.Monkey.positions);
+        // await dataContract.addDNAVariant(address, 0, DNA.DOG, data.DNA.Dog.names, data.DNA.Dog.traits, data.DNA.Dog.positions);
+        // await dataContract.addDNAVariant(address, 0, DNA.CAT, data.DNA.Cat.names, data.DNA.Cat.traits, data.DNA.Cat.positions);
+        // await dataContract.addDNAVariant(address, 0, DNA.FROG, data.DNA.Frog.names, data.DNA.Frog.traits, data.DNA.Frog.positions);
+        // await dataContract.addDNAVariant(address, 0, DNA.ROBOT, data.DNA.Robot.names, data.DNA.Robot.traits, data.DNA.Robot.positions);
+        // await dataContract.addDNAVariant(address, 0, DNA.HUMAN, data.DNA.Human.names, data.DNA.Human.traits, data.DNA.Human.positions);
+        // await dataContract.addDNAVariant(address, 0, DNA.MONKEY, data.DNA.Monkey.names, data.DNA.Monkey.traits, data.DNA.Monkey.positions);
 
     } catch (error) {
         console.log("Error checking positions:", error);
