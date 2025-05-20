57bf00789866c38f17dab80a475209af85fca768
142856 <142856eai@gmail.com>
2025-05-20 18:32:36 +0700

    remove mint in eai721art

--- contracts/interfaces/IEAI721Art.sol
+++ contracts/interfaces/IEAI721Art.sol
@@ -14,22 +14,6 @@ interface IEAI721Art {
         uint256 dna,
         uint256[5] memory traits
     ) external payable;
-
-    /**
-     * @dev Mints a new token to the specified address with the given dna, traits, agent name, and agent ability.
-     * @param to The address to which the new token will be minted.
-     * @param dna The DNA value for the new token.
-     * @param traits An array of traits for the new token.
-     * @param agentName The name of the agent associated with the token.
-     * @param agentAbility The ability of the agent associated with the token.
-     */
-    function mint(
-        address to, 
-        uint256 dna, 
-        uint256[5] memory traits, 
-        string calldata agentName, 
-        string calldata agentAbility
-    ) external;
      
     /**
      * @dev Returns the address of the CryptoAI data contract.
