212f19c9cb61f172dcb3880b417c50ef86cce067
dr.cat911 <dr.cat911@bvm.network>
2025-05-20 13:59:08 +0700

    Remove collections_error.json and update error handling in mint-auto_v3.ts: Deleted the collections_error.json file to streamline data management. Enhanced error handling by unlinking the error file when no errors are present, improving overall efficiency in the NFT minting process.

--- migrations/nfts/cryptoai/8.2.mint-auto_v3.ts
+++ migrations/nfts/cryptoai/8.2.mint-auto_v3.ts
@@ -176,6 +176,10 @@ async function main() {
         const collectionPathError = "migrations/data/cryptoai/datajson/collections_error.json";
        await fs.writeFile(collectionPathError, JSON.stringify(errorData, null, 2));
        console.log('____has Error:', errorData);
+    } else {
+    const collectionPathError = "migrations/data/cryptoai/datajson/collections_error.json";
+    await fs.unlink(collectionPathError);
+    console.log('____removed error file');
     }
    
     console.log('___key_dna_compress_data', key_input_render)
